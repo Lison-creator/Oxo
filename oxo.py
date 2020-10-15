2cd252ed033270e9c0a34f1158d8b525bdeb4563
Lison <lison@MacBook-Pro.local>
2020-10-15 15:35:07 +0200

    update-03

--- oxo.py
+++ oxo.py
@@ -16,15 +16,9 @@ def play():
     #demander une position en x et en y, vérifier que la position est vide, puis retourner le symb du joueur
     colonne = input("colonne(0-4): " )
     colonne = int(colonne)
-    if colonne > 4 and colonne < 0:
-        print("Votre placement est hors tableau. La position est comprise entre 0 et 4.")
-        colonne = input("colonne(0-4): " )
     
     ligne = input("ligne(0-4): ")
     ligne = int(ligne)
-    if ligne > 4 and ligne < 0:
-        print("Votre placement est hors tableau. La position est comprise entre 0 et 4.")
-        ligne = input("ligne(0-4): " )
 
     while give(colonne, ligne) != ".":
         print(f"la coordonnée {colonne},{ligne} n'est pas dispo !")
@@ -36,8 +30,25 @@ def play():
     return colonne, ligne
         
 def win():
-    win = False
-    if play()[0] == tableau[y]
+    player = give(x,y)
+
+    if give(x + 1, y) == player:
+        if give(x +2, y) == player:
+            return True
+        if give(x - 1, y) == player:
+        return True
+    if give(x - 1, y ) == player and give(x - 2, y) == player:
+        return True
+
+    if give(x, y + 1) == player:
+        if give(x, y + 2) == player:
+            return True
+        if give(x, y -1) == player:
+            return True
+    if give(x, y - 1) == player and give(x, y - 2) == player:
+        return True
+
+    return False
 
 def display():
     txt = " 0 1 2 3 4 \n"
@@ -53,6 +64,8 @@ def place(carac, x, y):
     pass
 
 def give(x,y):
+    if x < 0 or x > 4 or y < 0 or y > 4:
+        return None
     return tableau[y][x]
 
 def init():
@@ -71,13 +84,43 @@ def full():
 
 # Le script commence ici
 
-place("x", 3, 2)
 display()
 
+victoire = False
+
+turn = 0
+print("-= Init Round =-")
 x, y = play()
+while True:
 
-place(player1, x, y)
-place(player2, x, y)
+    full()
+    if full() == True:
+        print("Match Nul! :grimacing: ")
+        init()
+    
+    while turn == 0:            
+        
+        place(PLAYER_1, x, y)
+        display()
+        turn = 1
 
-
-
+    while turn == 1 and win(x, y) == False:
+        print("-= Player Two =-")
+        player = "Player Two"
+        x, y = play()
+        place(player1, x, y)
+        display()
+        win(x, y)
+        turn = 2
+    while turn == 2 and win(x, y) == False:
+        print("-= Player One =- ")
+        player = "Player One"
+        x, y = play()
+        place(player2, x, y)
+        display()
+        check_victory(x, y)
+        turn = 1
+    if check_victory(x, y) == True:
+        print(f"{player} Wins !")
+    
+        
