61fad55a5f34bf8d2c23f0977f047fe15fca2504
Lison <lison@MacBook-Pro.local>
2020-10-15 14:28:16 +0200

    update-2

--- oxo.py
+++ oxo.py
@@ -11,8 +11,36 @@ tableau = [
 
 # Fonctions
 
+def play():
+    #code qui permet à l'utilisateur de choisir une position valide en x et y
+    #demander une position en x et en y, vérifier que la position est vide, puis retourner le symb du joueur
+    colonne = input("colonne(0-4): " )
+    colonne = int(colonne)
+    if colonne > 4 and colonne < 0:
+        print("Votre placement est hors tableau. La position est comprise entre 0 et 4.")
+        colonne = input("colonne(0-4): " )
+    
+    ligne = input("ligne(0-4): ")
+    ligne = int(ligne)
+    if ligne > 4 and ligne < 0:
+        print("Votre placement est hors tableau. La position est comprise entre 0 et 4.")
+        ligne = input("ligne(0-4): " )
+
+    while give(colonne, ligne) != ".":
+        print(f"la coordonnée {colonne},{ligne} n'est pas dispo !")
+        colonne = input("colonne(0-4): ")
+        colonne = int(colonne)
+
+        ligne = input("ligne(0-4): ")
+        ligne = int(ligne)
+    return colonne, ligne
+        
+def win():
+    win = False
+    if play()[0] == tableau[y]
+
 def display():
-    txt = " 01234\n"
+    txt = " 0 1 2 3 4 \n"
     y = 0
     for ligne in tableau:
         for data in ligne:
@@ -46,5 +74,10 @@ def full():
 place("x", 3, 2)
 display()
 
-print(full())
+x, y = play()
+
+place(player1, x, y)
+place(player2, x, y)
+
+
 
